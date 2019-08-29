9e8d3b9cf90a3fc5f371ee203f8f33e070d72833
Landon Smith <landjbs@gmail.com>
2019-08-29 17:01:33 -0400

    discriminator naming

--- model.py
+++ model.py
@@ -18,34 +18,46 @@ RELU_ALPHA = 0.2
 # activation functions are used to give gradients to inactive units
 inputs = keras.layers.Input(shape=INPUT_SHAPE, name='inputs')
 # first conv block
-conv_1 = keras.layers.Conv2D(filters=(DEPTH*1), kernel_size=KERNEL_SIZE,
-                            strides=STRIDE, input_shape=INPUT_SHAPE,
-                            padding='same', name='conv_1')(inputs)
-relu_1 = keras.layers.Activation(keras.layers.LeakyReLU(RELU_ALPHA))(conv_1)
-drop_1 = keras.layers.Dropout(DROPOUT)(relu_1)
-# second conv block
-conv_2 = keras.layers.Conv2D(filters=(DEPTH*2), kernel_size=KERNEL_SIZE,
+conv_1 = keras.layers.Conv2D(filters=(DEPTH*1),
+                            kernel_size=KERNEL_SIZE,
                             strides=STRIDE,
-                            activation=keras.layers.LeakyReLU(RELU_ALPHA),
+                            input_shape=INPUT_SHAPE,
+                            padding='same',
+                            name='conv_1')(inputs)
+relu_1 = keras.layers.Activation(activation=keras.layers.LeakyReLU(RELU_ALPHA),
+                                name='relu_1')(conv_1)
+drop_1 = keras.layers.Dropout(DROPOUT, name='drop_1')(relu_1)
+# second conv block
+conv_2 = keras.layers.Conv2D(filters=(DEPTH*2),
+                            kernel_size=KERNEL_SIZE,
+                            strides=STRIDE,
                             padding='same',
                             name='conv_2')(drop_1)
-relu_2 = keras.layers.Activation(keras.layers.LeakyReLU(RELU_ALPHA))(conv_2)
-drop_2 = keras.layers.Dropout(DROPOUT)(relu_2)
+relu_2 = keras.layers.Activation(activation=keras.layers.LeakyReLU(RELU_ALPHA),
+                            name='relu_2')(conv_2)
+drop_2 = keras.layers.Dropout(DROPOUT, name='drop_2')(relu_2)
 # third conv block
-conv_3 = keras.layers.Conv2D(filters=(DEPTH*4), kernel_size=KERNEL_SIZE,
+conv_3 = keras.layers.Conv2D(filters=(DEPTH*4),
+                            kernel_size=KERNEL_SIZE,
                             strides=STRIDE,
-                            padding='same')(drop_2)
-relu_3 = keras.layers.Activation(keras.layers.LeakyReLU(RELU_ALPHA))(conv_3)
-drop_3 = keras.layers.Dropout(DROPOUT)(relu_3)
+                            padding='same',
+                            name='conv_3')(drop_2)
+relu_3 = keras.layers.Activation(activation=keras.layers.LeakyReLU(RELU_ALPHA),
+                                name='relu_3')(conv_3)
+drop_3 = keras.layers.Dropout(DROPOUT, name='drop_3')(relu_3)
 # fourth conv block
-conv_4 = keras.layers.Conv2D(filters=(DEPTH*8), kernel_size=KERNEL_SIZE,
+conv_4 = keras.layers.Conv2D(filters=(DEPTH*8),
+                        kernel_size=KERNEL_SIZE,
                             strides=STRIDE,
-                            activation=keras.layers.LeakyReLU(RELU_ALPHA),
-                            padding='same')(drop_3)
-relu_4 = keras.layers.Activation(keras.layers.LeakyReLU(RELU_ALPHA))(conv_4)
-drop_4 = keras.layers.Dropout(DROPOUT)(relu_4)
+                            padding='same',
+                            name='conv_4')(drop_3)
+relu_4 = keras.layers.Activation(activation=keras.layers.LeakyReLU(RELU_ALPHA),
+                                name='relu_4')(conv_4)
+drop_4 = keras.layers.Dropout(DROPOUT, name='drop_4')(relu_4)
 # convolutional output is flattened and passed to dense layer for classification
-flat = keras.layers.Flatten()(drop_4)
-outputs = keras.layers.Dense(units=1, activation='sigmoid')(flat)
+flat = keras.layers.Flatten(name='flat')(drop_4)
+outputs = keras.layers.Dense(units=1,
+                            activation='sigmoid',
+                            name='outputs')(flat)
 # build sequential model
 discriminator = keras.models.Model(inputs=inputs, outputs=outputs)
