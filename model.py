e3d7e1f09e874d669d07d957cbbb3e6722ce4cd5
Landon Smith <landjbs@gmail.com>
2019-08-29 18:07:29 -0400

    GAN inputs

--- model.py
+++ model.py
@@ -2,6 +2,17 @@
 Implements base model class for deep convolutional adversarial network
 """
 
+# def assert_types(obj, name, expectedType):
+#     """ Helper to assert proper typing of function inputs """
+#     assert isinstance(obj, expectedType), f'{name} expected type {expectedType}, but found type {type{obj}}'
+
 class GAN(object):
 
-    def __init__(self, rows, )
+    def __init__(self, name, rowNum, columnNum, channelNum):
+        self.name = name
+        self.rowNum = rowNum
+        self.columnNum = columnNum
+        self.channelNum = channelNum
+
+        self.discriminator = None
+        self.generator = None
