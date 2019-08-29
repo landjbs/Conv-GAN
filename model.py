efa163fea2e6862378331fd6d76aa10f29ce00ef
Landon Smith <landjbs@gmail.com>
2019-08-29 16:55:05 -0400

    model activations changed

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import keras
 # input is (x_pixels, y_pixels, channels), batch_size dropped
-INPUT_SHAPE = features.shape[:1]
+INPUT_SHAPE = features.shape[1:]
 # filter depth of the model
 DEPTH = 64
 # dropout of the model; high dropout should prevent memorization in generator
@@ -16,18 +16,19 @@ RELU_ALPHA = 0.2
 # convolutional layers have ((2**i) * DEPTH) filter number for i in range(0,4),
 # use strides of 2 for downsampling, and pad to match input shape. Leaky ReLU
 # activation functions are used to give gradients to inactive units
-inputs = keras.layers.Input(shape=INPUT_SHAPE)
+inputs = keras.layers.Input(shape=INPUT_SHAPE, name='inputs')
 # first conv block
 conv_1 = keras.layers.Conv2D(filters=(DEPTH*1), kernel_size=KERNEL_SIZE,
                             strides=STRIDE, input_shape=INPUT_SHAPE,
-                            activation=keras.layers.LeakyReLU(RELU_ALPHA),
-                            padding='same')(input)
-drop_1 = keras.layers.Dropout(DROPOUT)(conv_1)
+                            padding='same', name='conv_1')(inputs)
+relu_1 = keras.layers.Activation(keras.layers.LeakyReLU(RELU_ALPHA))(conv_1)
+drop_1 = keras.layers.Dropout(DROPOUT)(relu_1)
 # second conv block
 conv_2 = keras.layers.Conv2D(filters=(DEPTH*2), kernel_size=KERNEL_SIZE,
                             strides=STRIDE,
                             activation=keras.layers.LeakyReLU(RELU_ALPHA),
-                            padding='same')(drop_1)
+                            padding='same',
+                            name='conv_2')(drop_1)
 drop_2 = keras.layers.Dropout(DROPOUT)(conv_2)
 # third conv block
 conv_3 = keras.layers.Conv2D(filters=(DEPTH*4), kernel_size=KERNEL_SIZE,
