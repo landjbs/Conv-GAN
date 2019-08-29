ddf5d7918e2a128aba2dfb6b071a3cd1d8ca2c1c
Landon Smith <landjbs@gmail.com>
2019-08-29 18:03:04 -0400

    model class init

--- generator.py
+++ generator.py
@@ -14,12 +14,12 @@ NORM_MOMENTUM = 0.9
 
 get_filter_num = lambda depthMult : int(GEN_DEPTH / depthMult)
 
-latent = keras.layers.Input(shape=(GEN_INPUT_DIM, ), name='latent')
+latent_inputs = keras.layers.Input(shape=(GEN_INPUT_DIM, ), name='latent_inputs')
 
 # dense layer to adjust latent space
 dense_latent = keras.layers.Dense(units=((GEN_DIM**2) * GEN_DEPTH),
                         input_dim=GEN_INPUT_DIM,
-                        name='latent_dense')(latent)
+                        name='latent_dense')(latent_inputs)
 batch_dense = keras.layers.BatchNormalization(momentum=NORM_MOMENTUM)(dense_latent)
 relu_dense = keras.layers.Activation(activation=keras.layers.ReLU())(batch_dense)
 
@@ -31,7 +31,7 @@ latent_dropout = keras.layers.Dropout(DROPOUT)(latent_reshaped)
 # first upsampling block
 upsample_1 = keras.layers.UpSampling2D()(latent_dropout)
 # transpose convolution on upsampled latent dimensions
-transpose_1 = keras.layers.Conv2DTranspose(filters=get_filter_num(1),
+transpose_1 = keras.layers.Conv2DTranspose(filters=get_filter_num(2),
                                         kernel_size=KERNEL_SIZE,
                                         padding='same',
                                         name='transpose_1')(upsample_1)
@@ -40,11 +40,26 @@ relu_1 = keras.layers.Activation(activation=keras.layers.ReLU())(batch_1)
 
 # second upsampling block
 upsample_2 = keras.layers.UpSampling2D()(relu_1)
-transpose_2 = keras.layers.Conv2DTranspose(filters=get_filter_num(2),
+transpose_2 = keras.layers.Conv2DTranspose(filters=get_filter_num(4),
                                         kernel_size=KERNEL_SIZE,
                                         padding='same',
                                         name='transpose_2')(upsample_2)
 batch_2 = keras.layers.BatchNormalization(momentum=NORM_MOMENTUM)(transpose_2)
 relu_2 = keras.layers.Activation(activation=keras.layers.ReLU())(batch_2)
 
-# third upsampling block
+# third upsampling block: no upsampling for now
+transpose_3 = keras.layers.Conv2DTranspose(filters=get_filter_num(8),
+                                        kernel_size=KERNEL_SIZE,
+                                        padding='same',
+                                        name='transpose_3')(relu_2)
+batch_3 = keras.layers.BatchNormalization(momentum=NORM_MOMENTUM)(transpose_3)
+relu_3 = keras.layers.Activation(activation=keras.layers.ReLU())(batch_3)
+
+# sigmoid activation on final output to assert grayscale output in range [0, 1]
+output_transpose = keras.layers.Conv2DTranspose(filters=1,
+                                            kernel_size=5,
+                                            padding='same',
+                                            name='output_transpose')(relu_3)
+outputs = keras.layers.Activation('sigmoid')
+
+model = keras.models.Model(inputs=latent_inputs, outputs=output)
---
+++ model.py
@@ -0,0 +1,7 @@
+"""
+Implements base model class for deep convolutional adversarial network
+"""
+
+class GAN(object):
+
+    def __init__(self, rows, )
